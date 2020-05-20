8ec86431c6a65ac1756287223b4ad122dbf78f43
littleLane <lzz857183384@163.com>
2020-05-20 10:00:52 +0800

    learn: Vue core entry

--- src/core/instance/index.js
+++ src/core/instance/index.js
@@ -11,13 +11,25 @@ function Vue (options) {
   ) {
     warn('Vue is a constructor and should be called with the `new` keyword')
   }
+
+  // 通过 initMixin(Vue) 挂载
+  // reference: src/core/instance/init.js
   this._init(options)
 }
 
+// 初始化 Vue.prototype._init
 initMixin(Vue)
+
+// 向 Vue.prototype 混入设置状态相关的方法
 stateMixin(Vue)
+
+// 向 Vue.prototype 混入发布订阅（自定义事件机制）方法
 eventsMixin(Vue)
+
+// 向 Vue.prototype 混入生命周期相关的方法
 lifecycleMixin(Vue)
+
+// 向 Vue.prototype 混入渲染相关的方法
 renderMixin(Vue)
 
 export default Vue
