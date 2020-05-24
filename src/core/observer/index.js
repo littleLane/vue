e5c45b52938468890ca0325c6cd7e986dd659c5a
littleLane <lzz857183384@163.com>
2020-05-24 21:24:06 +0800

    learn: $set & $delete

--- src/core/observer/index.js
+++ src/core/observer/index.js
@@ -246,67 +246,93 @@ export function defineReactive (
  * Set a property on an object. Adds the new property and
  * triggers change notification if the property doesn't
  * already exist.
+ * call reference: src/core/global-api/index.js
+ * this.$set(obj, 'name', 'littleLane')
  */
 export function set (target: Array<any> | Object, key: any, val: any): any {
+  // target 必须是引用类型
   if (process.env.NODE_ENV !== 'production' &&
     (isUndef(target) || isPrimitive(target))
   ) {
     warn(`Cannot set reactive property on undefined, null, or primitive value: ${(target: any)}`)
   }
+
+  // target 是数组，根据 key 设置 length 属性
+  // 将 val 插入到合适的位置
+  // 返回 val
   if (Array.isArray(target) && isValidArrayIndex(key)) {
     target.length = Math.max(target.length, key)
     target.splice(key, 1, val)
     return val
   }
+
   if (key in target && !(key in Object.prototype)) {
     target[key] = val
     return val
   }
+
   const ob = (target: any).__ob__
-  if (target._isVue || (ob && ob.vmCount)) {
+  if (target._isVue /* Vue 实例 */ || (ob && ob.vmCount) /* root $data */) {
     process.env.NODE_ENV !== 'production' && warn(
       'Avoid adding reactive properties to a Vue instance or its root $data ' +
       'at runtime - declare it upfront in the data option.'
     )
     return val
   }
+
   if (!ob) {
     target[key] = val
     return val
   }
+
+  // 新添加的属性变成响应式对象
   defineReactive(ob.value, key, val)
+
+  // 手动的触发依赖更新通知
   ob.dep.notify()
   return val
 }
 
 /**
  * Delete a property and trigger change if necessary.
+ * call reference: src/core/global-api/index.js
+ *    this.$delete(obj, 'name')
  */
 export function del (target: Array<any> | Object, key: any) {
+  // target 必须是引用类型
   if (process.env.NODE_ENV !== 'production' &&
     (isUndef(target) || isPrimitive(target))
   ) {
     warn(`Cannot delete reactive property on undefined, null, or primitive value: ${(target: any)}`)
   }
+
+  // 数组直接调用 splice
   if (Array.isArray(target) && isValidArrayIndex(key)) {
     target.splice(key, 1)
     return
   }
+
   const ob = (target: any).__ob__
-  if (target._isVue || (ob && ob.vmCount)) {
+  if (target._isVue /* Vue 实例 */ || (ob && ob.vmCount) /* root $data */) {
     process.env.NODE_ENV !== 'production' && warn(
       'Avoid deleting properties on a Vue instance or its root $data ' +
       '- just set it to null.'
     )
     return
   }
+
+  // 不是自有属性直接 return
   if (!hasOwn(target, key)) {
     return
   }
+
   delete target[key]
+
   if (!ob) {
     return
   }
+
+  // 手动的触发依赖更新通知
   ob.dep.notify()
 }
 
